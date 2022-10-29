ec04bbf36f87489cab3f2e2e5c112ad5141f3e0a
Aiden Mathews <98974983+SealyBoi@users.noreply.github.com>
2022-10-29 14:58:04 -0500

    Add files via upload

---
+++ Main/LinkedList.java
@@ -0,0 +1,94 @@
+package Main;
+
+public class LinkedList {
+	Node head;
+	
+	static class Node {
+		String[] data;
+		Node next;
+		Node (String[] d) {
+			data = d;
+			next = null;
+		}
+	}
+	
+	public void push(String[] new_data) {
+		Node new_node = new Node(new_data);
+		new_node.next = head;
+		head = new_node;
+	}
+	
+	public void deleteNode(String[] key) {
+		Node temp = head;
+		
+		if (temp != null && temp.data == key) {
+			head = temp.next;
+			return;
+		}
+		
+		if (temp == null) {
+			return;
+		}
+	}
+	
+	public void pop() {
+		deleteNode(head.data);
+	}
+	
+	public boolean isNull() {
+		if (head == null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public String Name() {
+		return head.data[0];
+	}
+	
+	public String GPA() {
+		return head.data[1];
+	}
+	
+	public String AS() {
+		return head.data[2];
+	}
+	
+	public String Tuition() {
+		return head.data[3];
+	}
+	
+	public String Pop() {
+		return head.data[4];
+	}
+	
+	public String Loc() {
+		return head.data[5];
+	}
+	
+	public String AR() {
+		return head.data[6];
+	}
+	
+	public String Pub() {
+		return head.data[7];
+	}
+	
+	public String Majors() {
+		return head.data[8];
+	}
+	
+	public String GR() {
+		return head.data[9];
+	}
+	
+	public String Schol() {
+		return head.data[10];
+	}
+	
+	public String Score() {
+		return head.data[11];
+	}
+	
+}
---
+++ Main/PreSortedStack.java
@@ -0,0 +1,76 @@
+package Main;
+
+public class PreSortedStack {
+	private static String[][] colleges;
+	private int count;
+	private int sizeOfArray;
+	static Evaluate eval = new Evaluate();
+	static LinkedList llist = new LinkedList();
+	
+	public PreSortedStack() {
+		colleges = new String[1][];
+		count = 0;
+		sizeOfArray = 1;
+	}
+	
+	public void addElement(String[] a) {
+		if (count == sizeOfArray) {
+			growSize();
+		}
+		colleges[count] = a;
+		count++;
+	}
+	
+	public void growSize() {
+		String[][] tmp = new String[sizeOfArray * 2][];
+		for (int i = 0; i < sizeOfArray; i++) {
+			tmp[i] = colleges[i];
+		}
+		colleges = tmp;
+		sizeOfArray = sizeOfArray * 2;
+	}
+	
+	public int Size() {
+		return sizeOfArray;
+	}
+	
+	public void sendToList() {
+		for (int i = 0; i < sizeOfArray - 1; i++) {
+			colleges[i][11] = "" + eval.evaluate(colleges[i]);
+		}
+		sort(0, count - 1);
+		for (int i = count - 1; i >= 0; i--) {
+			llist.push(colleges[i]);
+		}
+	}
+	
+	public void sort(int left, int right) {
+		if (left < right) {
+			int piv = partition(left, right);
+			sort(left, piv - 1);
+			sort(piv + 1, right);
+		}
+	}
+	
+	public static int partition(int left, int right) {
+		double piv = Double.parseDouble(colleges[right][11]);
+		int i = left - 1;
+		
+		for (int j = left; j < right; j++) {
+			if (Double.parseDouble(colleges[j][11]) >= piv) {
+				i++;
+				swap(i, j);
+			}
+		}
+		
+		swap(i + 1, right);
+		
+		return (i + 1);
+	}
+	
+	public static void swap(int i, int j) {
+		String[] tmp = colleges[i];
+		colleges[i] = colleges[j];
+		colleges[j] = tmp;
+	}
+}
